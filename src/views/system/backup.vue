30062d92d3521572e2a5b5e6e78bfd154c869d02
chenxin <5397976+chenxin16@user.noreply.gitee.com>
2020-02-18 21:48:12 +0800

    文件备份

--- src/views/system/backup.vue
+++ src/views/system/backup.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="app-container">
     <div class="red">
-      <svg-icon class="svg-icon"
-                icon-class="backup" />
+      <svg-icon class="svg-icon" icon-class="backup" />
+      <div class="back-block">
+        <div class="back-time">
+          <span>2019-12-23</span>
+        </div>
+        <div class="back-message">
+          <span>备份成功</span>
+        </div>
+      </div>
     </div>
+    <div class="clear"></div>
     <div class="blue">
-      <svg-icon class="svg-icon"
-                icon-class="backup" />
+      <svg-icon class="svg-icon" icon-class="backup" />
+      <div class="back-block">
+        <div class="back-time">
+          <span>2019-12-23</span>
+        </div>
+        <div class="back-message">
+          <span>失败</span>
+        </div>
+      </div>
     </div>
+    <div class="clear"></div>
     <div class="grey">
-      <svg-icon class="svg-icon"
-                icon-class="backup" />
+      <svg-icon class="svg-icon" icon-class="backup" />
+      <div class="back-block">
+        <div class="back-time">
+          <span>2019-12-23</span>
+        </div>
+        <div class="back-message">
+          <span>备份成功</span>
+        </div>
+      </div>
     </div>
+    <div class="clear"></div>
   </div>
 </template>
 
@@ -25,11 +49,37 @@
 }
 .red {
   color: #d81c03;
+  padding-top: 80px;
+  padding-left: 160px;
 }
 .blue {
   color: #1290d3;
+  padding-top: 40px;
+  padding-left: 160px;
 }
 .grey {
   color: #878887;
+  padding-top: 40px;
+  padding-left: 160px;
+}
+.svg-icon {
+  float: left;
+}
+.back-block {
+  width: 350px;
+  float: left;
+  margin-top: 10px;
+}
+.back-time {
+  width: 150px;
+  float: left;
+  margin-left: 30px;
+}
+.back-message {
+  width: 150px;
+  float: left;
+}
+.clear {
+  clear: both;
 }
 </style>
